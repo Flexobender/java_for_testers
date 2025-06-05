7e3f9dd2fc35ab12fb6001594785e7677786325f
Alexei Mishurovskiy <a_m@list.ru>
2025-06-05 18:10:40 +0300

    Добавлен тест удаления групп

--- addressbook_web_tests/src/test/java/GroupDeletionTests.java
+++ addressbook_web_tests/src/test/java/GroupDeletionTests.java
@@ -1,15 +1,15 @@
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.*;
 
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 import java.util.*;
 
-
-public class GroupCreationTests {
+public class GroupDeletionTests {
   private WebDriver driver;
   private Map<String, Object> vars;
   JavascriptExecutor js;
@@ -24,39 +24,21 @@ public class GroupCreationTests {
     driver.quit();
   }
   @Test
-  public void myGroupTest() {
+  public void delGr() {
     driver.get("http://localhost/addressbook/");
     driver.manage().window().setSize(new Dimension(1564, 823));
-    {
-      WebElement element = driver.findElement(By.name("user"));
-      Actions builder = new Actions(driver);
-      builder.moveToElement(element).clickAndHold().perform();
-    }
-    {
-      WebElement element = driver.findElement(By.name("user"));
-      Actions builder = new Actions(driver);
-      builder.moveToElement(element).perform();
-    }
-    {
-      WebElement element = driver.findElement(By.name("user"));
-      Actions builder = new Actions(driver);
-      builder.moveToElement(element).release().perform();
-    }
     driver.findElement(By.name("user")).click();
     driver.findElement(By.name("user")).sendKeys("admin");
     driver.findElement(By.name("pass")).click();
     driver.findElement(By.name("pass")).sendKeys("secret");
     driver.findElement(By.xpath("//input[@value=\'Login\']")).click();
     driver.findElement(By.linkText("groups")).click();
-    driver.findElement(By.name("new")).click();
-    driver.findElement(By.name("group_name")).click();
-    driver.findElement(By.name("group_name")).sendKeys("Group1 name");
-    driver.findElement(By.name("group_header")).click();
-    driver.findElement(By.name("group_header")).sendKeys("Group1 header");
-    driver.findElement(By.name("group_footer")).click();
-    driver.findElement(By.name("group_footer")).sendKeys("Group1 footer");
-    driver.findElement(By.name("submit")).click();
+
+    driver.findElement(By.name("selected[]")).click();
+    driver.findElement(By.xpath("(//input[@name=\'selected[]\'])[2]")).click();
+    driver.findElement(By.name("delete")).click();
     driver.findElement(By.linkText("group page")).click();
     driver.findElement(By.linkText("Logout")).click();
+
   }
 }
